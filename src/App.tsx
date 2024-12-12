7a91db8e671ddc47a077966a385f1d1ec2df1361
YamsTheBe <Mariamadiaw.a@hotmail.fr>
2024-12-12 17:21:26 +0100

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import PokemonCard from "./components/PokemonCard";
 function App() {
 	return (
 		<div>
+			<h1>Welcome to the Pokedex</h1>
 			<PokemonCard /> {}
 		</div>
 	);
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,12 +1,25 @@
-const PokemonCard = () => {
+const pokemonList = [
+	{
+		name: "bulbasaur",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	},
+	{
+		name: "mew",
+	},
+];
+
+function PokemonCard() {
+	const pokemon = pokemonList[0];
 	return (
-		<figure>
-			<img
-				src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-				alt="Bulbasaur"
-			/>
-			<figcaption> Bulbasaur </figcaption>
-		</figure>
+		<div className="pokemonCard">
+			<figure>
+				<img src={pokemon.imgSrc} alt={pokemon.name} />
+
+				<figcaption> {pokemon.name} </figcaption>
+			</figure>
+		</div>
 	);
-};
+}
+
 export default PokemonCard;

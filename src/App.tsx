198bafda374668b3bb8b9a45cc9511f144dc4122
YamsTheBe <Mariamadiaw.a@hotmail.fr>
2024-12-13 14:37:22 +0100

    This is my first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+
 import "./App.css";
 
 import PokemonCard from "./components/PokemonCard";
+import type React from 'react';
+
+const App: React.FC = () => {
 
 function App() {
 	return (
 		<div>
-	
-			<PokemonCard /> {}
+			<PokemonCard
+				name="bulbasaur"
+				imgScr="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+			/>
 		</div>
 	);
 }
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,23 +1,15 @@
-const pokemonList = [
-	{
-		name: "bulbasaur",
-		imgSrc:
-			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-	},
-	{
-		name: "mew",
-	},
-];
 
-function PokemonCard() {
-	const pokemon = pokemonList[1];
+function PokemonCard(props: { pokemon: { name: string; imgScr?: string } }) {
+	// ceci permet d'afficher l'objet props dans la console
+	console.log(props);
+	const pokemon = props.pokemon; // Accèder au Pokémon via props
 
 	return (
 		<div className="pokemonCard">
 			<figure>
-			{pokemon.imgSrc ? (
-        <img src={pokemon.imgSrc} alt={pokemon.name} />
-			) : (
+				{pokemon.imgScr ? (
+					<img src={pokemon.imgScr} alt={pokemon.name} />
+				) : (
 					<p>??? </p>
 				)}
 				<figcaption> {pokemon.name} </figcaption>

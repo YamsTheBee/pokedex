f7a50352f35ae844b7a98b8507cffbfb27efa6ce
YamsTheBe <Mariamadiaw.a@hotmail.fr>
2024-12-18 15:21:29 +0100

    This is my second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,64 @@
-import type React from "react";
+//import type React from "react";
+import { useState } from "react";
 import PokemonCard from "./components/PokemonCard";
 
-const App: React.FC = () => {
-	const pokemonList = [
-		{
-			name: "Bulbasaur",
-			imgSrc:
-				"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-		},
-		{
-			name: "Charmander",
-			imgSrc:
-				"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-		},
-		{
-			name: "Squirtle",
-			imgSrc:
-				"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-		},
-	];
+//const App: React.FC = () => {
 
-	const selectedPokemon = pokemonList[0];
+// ma liste de pokemon
+const pokemonList = [
+	{
+		name: "bulbasaur",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	},
+	{
+		name: "charmander",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+	},
+	{
+		name: "squirtle",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+	},
+	{
+		name: "pikachu",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+	},
+	{
+		name: "mew",
+	},
+];
+
+function App() {
+	//const selectedPokemon = pokemonList[0];
+	const [pokemonIndex, setPokemonIndex] = useState(0);
+
+	const handlePrevClick = () => {
+		setPokemonIndex(
+			(prevIndex) => (prevIndex - 1 + pokemonList.length) % pokemonList.length,
+		);
+	};
+
+	const handleNextClick = () => {
+		setPokemonIndex((prevIndex) => (prevIndex + 1) % pokemonList.length);
+	};
 
 	return (
 		<div>
-			<PokemonCard pokemon={selectedPokemon} />
+			<h1>Pokedex</h1>
+			<PokemonCard pokemon={pokemonList[pokemonIndex]} />
+			<div>
+				<button type="button" onClick={handlePrevClick}>
+					Précédent
+				</button>
+				<button type="button" onClick={handleNextClick}>
+					Suivant
+				</button>
+			</div>
 		</div>
 	);
-};
+}
 
 export default App;
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,21 +1,21 @@
 import type React from "react";
 
-interface pokemon {
+interface Pokemon {
 	name: string;
 	imgSrc?: string;
 }
 
 interface PokemonCardProps {
-	pokemon: pokemon;
+	pokemon: Pokemon;
 }
 
 const PokemonCard: React.FC<PokemonCardProps> = ({ pokemon }) => {
 	return (
 		<div className="pokemon-card">
 			<img
-				src={pokemon.imgSrc || "https://via.placeholder.com/150"}
-				alt={pokemon.name}
-				className="pokemon-image"
+    	src={pokemon.imgSrc}
+		alt= {pokemon.name}
+		className="Pokemon-image"
 			/>
 
 			<h2>{pokemon.name}</h2>
